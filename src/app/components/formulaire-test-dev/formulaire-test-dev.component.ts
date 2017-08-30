79cf488ba9e29b2b72db06b6cf9e1eda9ef98aa0
Purpleproject <31441069+Purpleproject@users.noreply.github.com>
2017-08-30 09:45:42 +0200

    09.45 sebastien

---
+++ src/app/components/formulaire-test-dev/formulaire-test-dev.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-test-dev',
+  templateUrl: './formulaire-test-dev.component.html',
+  styleUrls: ['./formulaire-test-dev.component.css']
+})
+export class FormulaireTestDevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
