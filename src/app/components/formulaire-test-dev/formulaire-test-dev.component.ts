f3ba9f950f69fc5fbe1a648845907194b9222a92
Purpleproject <31441069+Purpleproject@users.noreply.github.com>
2017-08-31 10:15:47 +0200

    10:15 seb

--- src/app/components/formulaire-test-dev/formulaire-test-dev.component.ts
+++ src/app/components/formulaire-test-dev/formulaire-test-dev.component.ts
@@ -1,3 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-test-dev',
+  templateUrl: './formulaire-test-dev.component.html',
+  styleUrls: ['./formulaire-test-dev.component.css']
+})
+export class FormulaireTestDevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+/*
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
@@ -25,3 +42,4 @@ describe('FormulaireTesteurComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+*/
