4d2322d4b6b79a6a63b3b64848222d6da6a302c8
Purpleproject <31441069+Purpleproject@users.noreply.github.com>
2017-08-31 09:59:31 +0200

    09:59 seb test dev

--- src/app/components/formulaire-test-dev/formulaire-test-dev.component.ts
+++ src/app/components/formulaire-test-dev/formulaire-test-dev.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-@Component({
-  selector: 'app-formulaire-test-dev',
-  templateUrl: './formulaire-test-dev.component.html',
-  styleUrls: ['./formulaire-test-dev.component.css']
-})
-export class FormulaireTestDevComponent implements OnInit {
+import { FormulaireTestDevComponent } from './formulaire-test-dev.component';
 
-  constructor() { }
+describe('FormulaireTesteurComponent', () => {
+  let component: FormulaireTestDevComponent;
+  let fixture: ComponentFixture<FormulaireTestDevComponent>;
 
-  ngOnInit() {
-  }
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FormulaireTestDevComponent ]
+    })
+    .compileComponents();
+  }));
 
-}
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormulaireTestDevComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/services/editeur.service.ts
+++ src/app/services/editeur.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { Editeur } from '../entities/editeur';
 
 @Injectable()
 export class EditeurService {
 
-  constructor() { }
+  constructor(private http:Http) { }
+
+//Methode obtenirEditeurs
+obtenirEditeurs() {
+  let link="http://localhost:8080/BugTracker2/mvc/user/listeEditeurs"
+  return this.http.get(link).map(res=> res.json());
+}
+
+//Methode creerEditeurs
+creerEditeurs(Editeur) {
+  let link="http://localhost:8080/BugTracker2/mvc/user/creerEditeur"
+    return this.http.post(link, Editeur);
+  }  
 
 }
