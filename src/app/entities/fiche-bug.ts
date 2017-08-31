a5c455b734d5c7fc1edfe3c279a2f443a4a99713
Kiara <31441069+Purpleproject@users.noreply.github.com>
2017-08-31 16:11:06 +0200

    16h09 : nath : fiche bug .ts et service.ts

--- src/app/entities/fiche-bug.ts
+++ src/app/entities/fiche-bug.ts
@@ -1,18 +1,23 @@
 import {Testeur} from './testeur';
+import { Test } from './test';
 
 export class FicheBug {
 
     constructor(id: Number, libelle: String, description: String,
-         statut: String) {
+         statut: String, testeur: Testeur, test: Test) {
 
         this.id = id;
         this.libelle= libelle;
         this.description=description;
         this.statut=statut;
+        this.testeur=testeur;
+        this.test=test;
 
     }
     id: Number;
     libelle: String; 
     description: String; 
-    statut: String; 
+    statut: String;
+    testeur: Testeur;
+    test:Test;
 }
