6addf82158aae8800ca07b95581abcfd9a1a5df8
Purpleproject <31441069+Purpleproject@users.noreply.github.com>
2017-08-31 15:47:35 +0200

    15:47 seb fichebug +module

---
+++ src/app/components/formulaire-fichebug/formulaire-fichebug.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-fichebug',
+  templateUrl: './formulaire-fichebug.component.html',
+  styleUrls: ['./formulaire-fichebug.component.css']
+})
+export class FormulaireFichebugComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
