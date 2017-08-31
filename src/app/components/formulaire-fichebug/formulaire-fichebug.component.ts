f711032bd5daf807c20f3eb2c9c927b965eb5c0a
Kiara <31441069+Purpleproject@users.noreply.github.com>
2017-08-31 17:05:41 +0200

    17h05 : nath liste bug

--- src/app/components/formulaire-fichebug/formulaire-fichebug.component.ts
+++ src/app/components/formulaire-fichebug/formulaire-fichebug.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FicheBug } from '../../entities/fiche-bug';
+import { FicheBugService } from '../../services/fiche-bug.service';
 
 @Component({
   selector: 'app-formulaire-fichebug',
   templateUrl: './formulaire-fichebug.component.html',
-  styleUrls: ['./formulaire-fichebug.component.css']
+  styleUrls: ['./formulaire-fichebug.component.css'],
+  styles: [`
+  .ng-valid { border-color: green; }
+  .ng-invalid { border-color: red; } `
+
+  ]
 })
 export class FormulaireFichebugComponent implements OnInit {
 
-  constructor() { }
+  private ficheBug : FicheBug = new FicheBug(0, "","", "Non Traité", null, null);
+
+  constructor(private ficheBugService : FicheBugService) { }
+
+  onSubmit(): void {
+    
+        console.log(this.ficheBug.libelle);
+        //appel du service :
+        this.ficheBugService.creerFicheBug(this.ficheBug)
+          .subscribe(res => {console.log("appel envoyé : "+res);});
+        console.log(this.ficheBug.libelle)
+    
+      }
 
   ngOnInit() {
   }
